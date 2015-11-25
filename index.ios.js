5cc559800e0e186fa2a2dc3e74b96a3dbb429bc4
Alfosno <alfonso.ferandez@gmail.com>
2015-11-25 20:25:31 +0400

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} = React;
+
+var fullimage = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Image source={require("./cover.png") }  style={{ flex: 1 }}/>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('fullimage', () => fullimage);
